48c912a40862fcabd082e5513f313ddde2aebbf5
Moiz-Asad <moizasad86@gmail.com>
2021-05-26 15:33:12 +0500

    Added search functionality with custom styling.

--- src/Components/FoodItems.jsx
+++ src/Components/FoodItems.jsx
@@ -1,18 +1,56 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Headlogo from './Headlogo';
 import FoodDisplayCard from './FoodDisplayCard';
-
-function FoodItems(prop) {
+function FoodItems() {
     const Data = require("./Database/Recipee.json");
-    return (
-        <div className="container-fluid">
-        <div className="row">
-            {Data.map((dt)=>{
-            return (
-                <FoodDisplayCard key={dt.fid} fname={dt.name} url={dt.imageURL}/>);
-            })}
+    const [search, setsearch] = useState("");
+    const [state, setstate] = useState("");
+    const SearchItem=(event)=>{
+        setsearch(event.target.value)
+    }
+    const SearchBarDisplay = ()=>{
+        setstate("searchstate");
+    }
+    if(state == "searchstate")
+    {
+        return (<div className="container-fluid" className="filled">
+        <Headlogo/>
+        <div className="form-outline">
+        
+            <input type="search" className="form-control" value={search} onChange={SearchItem} placeholder="Search Food" autoFocus="true" aria-label="Search" />
         </div>
+                
+                <div className="row">
+                {Data.map((dt)=>{
+                    var s1 = dt.name.toLowerCase();
+                    var s2 = search.toLowerCase();
+                    if(s1.includes(s2))
+                    {
+                        return (<FoodDisplayCard key={dt.fid} fname={dt.name} url={dt.imageURL}/>);
+                    }
+                })}
+                </div>
+
         </div>
-    )
+        )
+    }
+    else
+    {
+        return (
+            
+            <div className="container-fluid" className="filled">
+            <Headlogo/>
+            <button className="btn btn-light" onClick={SearchBarDisplay}>🔍</button>
+            
+            <div className="row">
+                {Data.map((dt)=>{
+                return (
+                    <FoodDisplayCard key={dt.fid} fname={dt.name} url={dt.imageURL}/>);
+                })}
+            </div>
+            </div>
+        )    
+    }
 }
 
 export default FoodItems
---
+++ src/Components/Headlogo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Headlogo() {
+    return (
+        <div className="container-fluid" className="headlg">
+        <h1 className="logo">Food Recipee React App</h1>
+            
+        </div>
+    )
+}
+
+export default Headlogo
